6ff3b0cf697ffbc94e63862d646352a9a38039f3
milandrapic <55461949+milandrapic@users.noreply.github.com>
2021-04-05 13:28:41 -0400

    Add files via upload

---
+++ src/Checkout/Checkout.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+class Checkout extends Component {
+
+    render(){
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+
+}
+
+export default Checkout;
